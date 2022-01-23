34c7fce4602cb2350e29bdb2a75a79abedd55fea
l bates childs <labchilds@gmail.com>
2022-01-23 00:15:37 -0500

    added post-routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 
 router.use('/users', userRoutes);
+router.use('/posts', require('./post-routes'));
 
 module.exports = router;
