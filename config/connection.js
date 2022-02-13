1ce0167da3ae1f00fe60dea2e6fdf7c2f01a41c0
l bates childs <labchilds@gmail.com>
2022-02-13 13:56:32 -0500

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -2,12 +2,17 @@
 const Sequelize = require('sequelize');
 // import sensitive login vars
 require('dotenv').config();
+let sequelize;
 
 // create connection to datatbase with mysql info
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else{
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+}
 
 module.exports = sequelize;
