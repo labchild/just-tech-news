a1e7d08220205085288efdc775df1e98a0e1eada
l bates childs <labchilds@gmail.com>
2022-02-14 17:46:40 -0500

    add comment form handler and update comment route to take new data

--- controllers/apiRoutes/comment-routes.js
+++ controllers/apiRoutes/comment-routes.js
@@ -13,17 +13,19 @@ router.get('/', (req, res) => {
 
 // create comment
 router.post('/', (req, res) => {
-    Comment.create({
-        comment_text: req.body.comment_text,
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
-    })
-        .then(dbCommentData => res.json(dbCommentData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
+    if (req.session) {
+        Comment.create({
+            comment_text: req.body.comment_text,
+            user_id: req.session.user_id,
+            post_id: req.body.post_id
         })
-})
+            .then(dbCommentData => res.json(dbCommentData))
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    }
+});
 
 // delete comment
 router.delete('/:id', (req, res) => {
