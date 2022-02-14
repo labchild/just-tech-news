946a558b8af44b1c1dcee73ac8f83a08b1d1c936
l bates childs <labchilds@gmail.com>
2022-02-14 14:36:28 -0500

    add login view, public login script

--- controllers/apiRoutes/user-routes.js
+++ controllers/apiRoutes/user-routes.js
@@ -68,10 +68,7 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-        .then(dbUserData => {
-            console.log('working!');
-            res.json(dbUserData);
-        })
+        .then(dbUserData => res.json(dbUserData))
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
