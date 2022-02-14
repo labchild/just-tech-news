38d1af9449b4bc79958ee7bbcb3abb3794b77eb3
l bates childs <labchilds@gmail.com>
2022-02-14 15:20:54 -0500

    add logout handler

--- controllers/apiRoutes/user-routes.js
+++ controllers/apiRoutes/user-routes.js
@@ -68,7 +68,15 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-        .then(dbUserData => res.json(dbUserData))
+        .then(dbUserData => {
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
+
+                res.json(dbUserData);
+            });
+        })
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
@@ -94,9 +102,26 @@ router.post('/login', (req, res) => {
             return;
         }
 
-        res.json({ user: dbUserData, message: 'You are now logged in!' });
+        req.session.save(() => {
+            req.session.id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
+        })
     });
 });
+
+// post to logout/destroy session
+router.post('/logout', (req,res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+})
 
 // put/update user by id
 router.put('/:id', (req, res) => {
--- server.js
+++ server.js
@@ -16,6 +16,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+  secret: 'bottleBlonde',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+app.use(session(sess));
+
 app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
